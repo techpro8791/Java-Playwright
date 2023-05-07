45755f236e33f2cec35522ed68f0debeec510599
techpro8791 <techpro8791@gmail.com>
2023-05-07 21:39:27 +0100

    Class and Methods Tutorial

---
+++ test/Demo.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        // main method
+        Groceries groceries = new Groceries(); // creating an instance so that it can view all objects of the class
+        groceries.Beverages();
+        groceries.Fruits();
+        groceries.Snacks();
+        groceries.Vegetables();
+
+    }
+
+}
