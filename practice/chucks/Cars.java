9f955f24288d169b5f8033fa4facf385e49a8130
Chuks22 <Datigas@gmail.com>
2023-05-14 16:10:20 +0100

    Final anwer on assignment

---
+++ practice/chucks/Cars.java
@@ -0,0 +1,19 @@
+package practice.chucks;
+
+public class Cars {
+    
+    public static void main (String[] args) {
+        // main method
+        LuxuaryCars cars = new LuxuaryCars();
+         cars.Benz();
+         cars.Lamborghini();
+         cars.Bentley();
+         cars.Jaguar();
+    }
+       
+          } 
+
+
+      
+          
+
---
+++ practice/chucks/LuxuaryCars.java
@@ -0,0 +1,75 @@
+package practice.chucks;
+
+public class LuxuaryCars {
+
+
+    public void Benz() {
+       
+        String GLE  ="2016";
+        String CClass  ="2019";
+        String GLC = "2016";
+        String AMG = "2018";
+        String GLB = "2020";
+     
+     System.out.println(GLE + " " + CClass + " " + GLC + " " + AMG + " " + GLB);
+     } 
+      
+
+
+      public void Lamborghini() {
+     
+          String Aventador  ="2016";
+          String Huracan  ="2019";
+          String Urus = "2016";
+          String Countach = "2018";
+          String Diablo = "2020";
+       
+       System.out.println(Aventador + " " + Huracan + " " + Urus + " " + Countach + " " + Diablo);
+      }
+  
+  public void Bentley() {
+     
+      String Bentayga  ="2016";
+      String Continental   ="2019";
+      String Flying  = "2016";
+      String Mulsanne = "2018";
+      String Bacalar = "2020";
+   
+   System.out.println(Bentayga + " " + Continental + " " + Flying  + " " + Mulsanne + " " + Bacalar);
+  }  
+  
+
+  public void Jaguar() {
+     
+      String FType  ="2016";
+      String XE   ="2019";
+      String XF  = "2016";
+      String EPACE = "2018";
+      String XJ = "2020";
+   
+   System.out.println(FType + " " + XE  + " " + XF  + " " + EPACE + " " + XJ);
+  }  
+
+
+  public void Maserati() {
+     
+      String Ghibli  ="2016";
+      String Quattroporte  ="2019";
+      String Levante  = "2016";
+      String GranTurismo = "2018";
+      String GranCabrio = "2020";
+   
+   System.out.println(Ghibli + " " + Quattroporte  + " " + Levante  + " " + GranTurismo + " " + GranCabrio);
+  } 
+  public void Ferrari() {
+     
+      String Italia  ="2016";
+      String California  ="2019";
+      String LaFerrari  = "2016";
+      String Portofino = "2018";
+      String Roma = "2020";
+   
+   System.out.println(Italia + " " + California  + " " + LaFerrari  + " " + Portofino + " " + Roma);
+  } 
+    
+}
