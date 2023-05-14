5fd315b137d4906f8c4832952dcd372309485f97
Aloynath <aloynath@gmail.com>
2023-05-14 14:04:07 +0100

    Assignment submission

---
+++ practice/aloy/FamilyTree.java
@@ -0,0 +1,16 @@
+package practice.aloy;
+
+public class FamilyTree {
+    
+    public static void main (String [] args){
+
+        Persons person = new Persons();
+        person.FirstGeneration();
+        person.SecondGeneration();
+        person.ThirdGeneration();
+        person.FourthGeneration();
+        person.FifthGeneration();
+        person.SixthGeneration();
+
+    }
+}
